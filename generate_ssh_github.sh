e4e2464f3653210f4b3c4f003b5a8749aaa33dd9
ruincorn <lugushik@gmail.com>
2026-01-08 20:52:26 +0300

    Add script for generating ssh for github.com

---
+++ generate_ssh_github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter email: " email
+ssh-keygen -t ed25519 -C "$email"
+read -p "Enter path where you stored keys: " path
+ssh-add "$path"
+killall ssh-agent
+ssh-agent
+echo "Now try visiting https://docs.github.com/en/authentication/troubleshooting-ssh/error-host-key-verification-failed"
